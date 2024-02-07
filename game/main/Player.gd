66ab1cb220e77c1847279c64a574b07f3b8d42cb
LeFias <LeFias@ya.ru>
2024-02-08 04:05:08 +0500

    testing and returning icon

--- game/main/Player.gd
+++ game/main/Player.gd
@@ -6,11 +6,17 @@ extends RigidBody2D
 const SPEED := 300.0
 
 func _process(delta):
-	var mouse_pos := get_viewport().get_mouse_position()
-	sprite.look_at(mouse_pos + global_position - get_viewport().get_visible_rect().size/2)
-	sprite.rotate(PI)
+	pass
 
 func _physics_process(delta):
 	var direction := Input.get_vector("ui_left", "ui_right", "ui_up","ui_down")
 	apply_force(direction * SPEED)
+	
+	var mouse_pos := get_global_mouse_position()
+	#look_at(mouse_pos)
+	rotation = get_global_mouse_position().angle_to_point(position)
+	#rotate(PI)
+	# ЗАМЕНИТЬ НА ФИЗ ВРАЩЕНИЕ
+	
 
+	
